e6dd304457e0dff4b0eedb3cdac232b11a904608
errphoenix <verycoolguy76@gmail.com>
2025-07-02 16:04:37 +0200

    chore: bump version `0.2.0` -> `0.2.1`

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function Header() {
                      }
                  }}>
                 <div className="text-gray-500 text-sm p-2 select-none">
-                    v0.2.0
+                    v0.2.1
                 </div>
                 <div
                     className="hover:bg-red-500 duration-300 p-2 mr-2 rounded cursor-pointer"
